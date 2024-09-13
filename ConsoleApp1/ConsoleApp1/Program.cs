43894162b6563e0b2538ceef10933f478bd75adf
Sonu Thapa <37073011+mesailesh7@users.noreply.github.com>
2024-09-13 10:12:24 -0600

    Initial commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// WorkWithIntegeres();
+
+void WorkWithIntegeres()
+{
+    int a = 18;
+    int b = 6;
+    int c = a + b;
+    Console.WriteLine(c);
+    
+    //substraction
+    c = a - b;
+    Console.WriteLine(c);
+    
+    //multiplication
+    c = a * b;
+    Console.WriteLine(c);
+    
+    //division
+    c = a / b;
+    Console.WriteLine(c);
+}
+
+
+
+int a = 5;
+int b = 6;
+int c = 2;
+// int d = (a + b) * c;
+int d = (a + b) - 6 * c + (12 * 4) / 3 + 12;
+Console.WriteLine(d);
+
